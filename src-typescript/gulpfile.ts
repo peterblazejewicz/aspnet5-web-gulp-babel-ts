c479795d8f006fc8803211de0fbd234970e2bc1d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-17 23:17:23 +0200

    Update TypeScript implementation
    WIP

--- src-typescript/gulpfile.ts
+++ src-typescript/gulpfile.ts
@@ -12,38 +12,51 @@ import uglify = require("gulp-uglify");
 var project = require("./project.json");
 import mainBowerFiles = require("gulp-main-bower-files");
 
-var paths = {
-  webroot: string;
-  concactJsDest: string;
-  concatCssDest: string;
-  css: string,
-  js: string,
-  lib: string,
-  minCss: string,
-  minJs: string
-} = {
-  webroot: "./" + project.webroot + "/",
-  concatCssDest:  paths.webroot + "css/site.min.css",
-  concatJsDest: paths.webroot + "js/site.min.js",
-  css: paths.webroot + "css/**/*.css",
-  js: paths.webroot + "js/**/*.js",
-  lib: paths.webroot + "lib/",
-  minCss: paths.webroot + "css/**/*.min.css",
-  minJs: paths.webroot + "js/**/*.min.js"
+var webroot:string = "./" + project.webroot + "/";
+var paths:any = {
+  js: webroot + "js/**/*.js",
+  minJs: webroot + "js/**/*.min.js",
+  css: webroot + "css/**/*.css",
+  minCss: webroot + "css/**/*.min.css",
+  concatJsDest: webroot + "js/site.min.js",
+  concatCssDest: webroot + "css/site.min.css",
+  lib: webroot + "lib/"
 };
 
-gulp.task("default", () => {
-  console.log("hello");
+gulp.task("clean:js", (cb) => {
+  rimraf(paths.concatJsDest, cb);
 });
 
-gulp.task("bower", () => {
-  console.log("bower task");
+gulp.task("clean:css", (cb) => {
+  rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task("clean", () => {
-  console.log("clean task");
+gulp.task("clean:lib", (cb) => {
+  rimraf(paths.lib, cb);
 });
 
-gulp.task("min", () => {
-  console.log("min task");
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", () => {
+  gulp.src([paths.js, "!" + paths.minJs], {
+      base: ".",
+    })
+    .pipe(concat(paths.concatJsDest))
+    .pipe(uglify())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", () => {
+  gulp.src([paths.css, "!" + paths.minCss])
+    .pipe(concat(paths.concatCssDest))
+    .pipe(cssmin())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("bower", ["clean:lib"], () => {
+  return gulp.src("./bower.json")
+    .pipe(mainBowerFiles())
+    .pipe(gulp.dest(paths.lib));
 });
